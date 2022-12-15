bb4a2c942e7283497cf81761ba9b5403089aba52
ilalpradhan <ilalpradhan@gmail.com>
2022-12-15 00:20:23 -0500

    frontend UI initial setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/DashBoard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Header from "./components/Header";
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<>
+			<Router>
+				<div className="container">
+					<Header />
+					<Routes>
+						<Route path="/" element={<Dashboard />}></Route>
+						<Route path="/login" element={<Login />}></Route>
+						<Route path="/register" element={<Register />}></Route>
+					</Routes>
+				</div>
+			</Router>
+		</>
+	);
 }
 
 export default App;
